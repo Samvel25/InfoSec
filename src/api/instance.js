0b470cd54d41961495c8d95eacb7b7a04542205b
Mihran2001 <xachatryan200104@gmail.com>
2023-07-23 19:09:25 +0400

    ...

---
+++ src/api/instance.js
@@ -0,0 +1,13 @@
+import axios from "axios";
+
+console.log("INSTANCE_URL:", process.env.INSTANCE_URL);
+
+const instance = axios.create({
+  //   baseURL: process.env.INSTANCE_URL,
+  baseURL: "https://localhost:9001/",
+  headers: {
+    "content-type": "application/json",
+  },
+});
+
+export default instance;
