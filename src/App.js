84b9ddae6c4c6b89ab99abc201b624e4139dc79d
Samvel25 <avagyan.samvel02@gmail.com>
2023-07-24 23:36:35 +0300

    log in admin add

--- src/App.js
+++ src/App.js
@@ -1,3 +1,70 @@
+// import React from "react";
+// import { createBrowserRouter, RouterProvider, Outlet } from "react-router-dom";
+// import { AnimatePresence, AnimateSharedLayout } from "framer-motion";
+// import Layout from "./components/Layout/Layout";
+// import { Stack } from "@mui/material";
+// import CssBaseline from "@mui/material/CssBaseline";
+// import "./index.css";
+// import About from "./pages/About/index";
+// import Services from "./pages/Services";
+// import Clients from "./pages/Clients";
+// import Certificates from "./pages/Certificates";
+// import Home from "./pages/Home";
+// import ScrollToTop from "./components/ScrollToTop"; // Import here
+// import Admin from "./pages/Admin";
+
+// const router = createBrowserRouter([
+// 	{
+// 		path: "/",
+// 		element: <Layout />,
+// 		children: [
+// 			{
+// 				path: "",
+// 				element: <Home />,
+// 			},
+// 			{
+// 				path: "about",
+// 				element: <About />,
+// 			},
+// 			{
+// 				path: "services",
+// 				element: <Services />,
+// 			},
+// 			{
+// 				path: "clients",
+// 				element: <Clients />,
+// 			},
+
+// 			{
+// 				path: "certificates",
+// 				element: <Certificates />,
+// 			},
+// 			{
+// 				path: "admin",
+// 				element: <Admin />,
+// 			},
+// 		],
+// 	},
+// ]);
+
+// function App() {
+// 	return (
+// 		<Stack id="app" height={"100vh"} sx={{ overflowX: "hidden" }}>
+// 			<CssBaseline />
+// 			<RouterProvider router={router}>
+// 				<ScrollToTop /> {/* Include it here */}
+// 				<AnimateSharedLayout>
+// 					<AnimatePresence mode="wait">
+// 						<Outlet />
+// 					</AnimatePresence>
+// 				</AnimateSharedLayout>
+// 			</RouterProvider>
+// 		</Stack>
+// 	);
+// }
+
+// export default App;
+
 import React from "react";
 import { createBrowserRouter, RouterProvider, Outlet } from "react-router-dom";
 import { AnimatePresence, AnimateSharedLayout } from "framer-motion";
@@ -12,6 +79,7 @@ import Certificates from "./pages/Certificates";
 import Home from "./pages/Home";
 import ScrollToTop from "./components/ScrollToTop"; // Import here
 import Admin from "./pages/Admin";
+import PrivateRoute from "./routes/PrivateRoute";
 
 const router = createBrowserRouter([
 	{
@@ -41,7 +109,11 @@ const router = createBrowserRouter([
 			},
 			{
 				path: "admin",
-				element: <Admin />,
+				element: (
+					<PrivateRoute path="/admin">
+						<Admin />
+					</PrivateRoute>
+				),
 			},
 		],
 	},
@@ -51,6 +123,7 @@ function App() {
 	return (
 		<Stack id="app" height={"100vh"} sx={{ overflowX: "hidden" }}>
 			<CssBaseline />
+
 			<RouterProvider router={router}>
 				<ScrollToTop /> {/* Include it here */}
 				<AnimateSharedLayout>
---
+++ src/routes/PrivateRoute/index.js
@@ -0,0 +1,14 @@
+import { Navigate, Route, useLocation } from "react-router-dom";
+
+const PrivateRoute = ({ children, path }) => {
+	const location = useLocation();
+	const userRole = localStorage.getItem("userRole");
+
+	return userRole === "admin" ? (
+		<Route path={path}>{children}</Route>
+	) : (
+		<Navigate to="/" state={{ from: location }} replace />
+	);
+};
+
+export default PrivateRoute;
