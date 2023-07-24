c348e41bbe51a748627bfb86b5da9178864eb8e2
Mihran2001 <xachatryan200104@gmail.com>
2023-07-25 01:14:00 +0400

    forms list page add data

--- src/routes/PrivateRoute/index.js
+++ src/routes/PrivateRoute/index.js
@@ -1,14 +1,15 @@
 import { Navigate, Route, useLocation } from "react-router-dom";
 
 const PrivateRoute = ({ children, path }) => {
-	const location = useLocation();
-	const userRole = localStorage.getItem("userRole");
+  const location = useLocation();
+  const userRole = localStorage.getItem("userRole");
 
-	return userRole === "admin" ? (
-		<Route path={path}>{children}</Route>
-	) : (
-		<Navigate to="/" state={{ from: location }} replace />
-	);
+  return userRole === "admin" ? (
+    // <Route path={path}>{children}</Route>
+    <Navigate to={path} state={{ from: location }} replace />
+  ) : (
+    <Navigate to="/" state={{ from: location }} replace />
+  );
 };
 
 export default PrivateRoute;
