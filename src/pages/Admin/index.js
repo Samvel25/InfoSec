8c857ee680b292fa6d7d2f3a399425f8d1ac088e
Samvel25 <avagyan.samvel02@gmail.com>
2023-09-03 02:48:19 +0300

    no message

--- src/pages/Admin/index.js
+++ src/pages/Admin/index.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 // import {
 // 	Box,
 // 	Grid,
@@ -474,20 +473,18 @@
 
 // export default Admin;
 
-=======
 import {
-  Box,
-  Grid,
-  TextareaAutosize,
-  Typography,
-  Dialog,
-  DialogActions,
-  DialogContent,
-  DialogContentText,
-  DialogTitle,
-  Button,
+	Box,
+	Grid,
+	TextareaAutosize,
+	Typography,
+	Dialog,
+	DialogActions,
+	DialogContent,
+	DialogContentText,
+	DialogTitle,
+	Button,
 } from "@mui/material";
->>>>>>> 12ac11f74c2b1f0d8386d22ee5795acf50afd965
 import React, { useEffect, useState } from "react";
 import { Navigate, useLocation } from "react-router-dom";
 import instance from "../../api/instance";
@@ -500,13 +497,12 @@ import MailSendTextArea from "./MailSendTextArea";
 import SubmitedForms from "./SubmitedForms";
 
 const Admin = () => {
-  const [formsData, setFormsData] = useState([]);
-  const user = useAuth((state) => state.user);
-  const location = useLocation();
-  const [isModalOpen, setIsModalOpen] = useState(false);
-  const [childComponent, setChildComponent] = useState(<></>);
+	const [formsData, setFormsData] = useState([]);
+	const user = useAuth((state) => state.user);
+	const location = useLocation();
+	const [isModalOpen, setIsModalOpen] = useState(false);
+	const [childComponent, setChildComponent] = useState(<></>);
 
-<<<<<<< HEAD
 	useEffect(() => {
 		instance.get("getAllForms").then((res) => {
 			const reversedData = [...res.data].reverse();
@@ -530,278 +526,6 @@ const Admin = () => {
 			<SubmitedForms />
 		</Container>
 	);
-=======
-  const [text, setText] = useState("");
-  const [showConfirm, setShowConfirm] = useState(false);
-
-  const handleTextChange = (e) => {
-    setText(e.target.value);
-  };
-
-  //   try {
-  // 	instance.post("/saveUserEmail", { email: text });
-  // 	}
-
-  const handleSendClick = () => {
-    setShowConfirm(true);
-  };
-
-  const handleConfirmSend = () => {
-    // try {
-    //   instance.post("/saveUserEmail", { email: text });
-    // } catch (err) {
-    //   console.log(err);
-    // }
-    setText("");
-
-    setShowConfirm(false);
-  };
-
-  const handleCancelSend = () => {
-    setShowConfirm(false);
-  };
-
-  const handleModalButtonClick = (childComponent) => {
-    setChildComponent(childComponent);
-    setIsModalOpen((prev) => !prev);
-  };
-  useEffect(() => {
-    instance.get("getAllForms").then((res) => {
-      const reversedData = [...res.data].reverse();
-      setFormsData(reversedData);
-    });
-  }, []);
-
-  console.log({ user });
-
-  if (user?.role !== "admin") {
-    return <Navigate to={"/"} state={{ from: location }} replace />;
-  }
-
-  console.log({ formsData });
-
-  return (
-    <div>
-      <Modal
-        handleClose={setIsModalOpen}
-        isopen={isModalOpen}
-        childComponent={childComponent}
-      />
-      <Styled.SubmitedForm
-        container
-        justifyContent={{ sm: "center", md: "space-around" }}
-        direction={"row"}
-        alignItems="center"
-        sx={{ m: "120px 0 130px" }}
-      >
-        <Grid item xs={12}>
-          <Grid container direction={"row"} justifyContent={"center"}>
-            <Grid sx={{ position: "relative" }} item xs={5}>
-              <Styled.StyledTextarea
-                value={text}
-                onChange={handleTextChange}
-                minRows={3}
-                placeholder="Add your comments here..."
-              />
-              <Styled.SendButton onClick={handleSendClick}>
-                Send
-              </Styled.SendButton>
-            </Grid>
-            <Dialog open={showConfirm} onClose={handleCancelSend}>
-              <DialogTitle>Confirm</DialogTitle>
-              <DialogContent>
-                <DialogContentText>
-                  Are you sure you want to send this message?
-                </DialogContentText>
-              </DialogContent>
-              <DialogActions>
-                <Button onClick={handleCancelSend} color="primary">
-                  Cancel
-                </Button>
-                <Button onClick={handleConfirmSend} color="primary">
-                  Yes, Send it!
-                </Button>
-              </DialogActions>
-            </Dialog>
-          </Grid>
-        </Grid>
-        {formsData.length
-          ? formsData.map((card, index) => (
-              <Grid item xs={11} sm={6} md={4} lg={2.5} xl={2} xxl={1.7}>
-                <Styled.Card
-                  sx={{ cursor: "pointer" }}
-                  key={index}
-                  boxpadding="boxpadding"
-                  backgroundtype="backgroundOne"
-                  onClick={() =>
-                    handleModalButtonClick(
-                      <ModalReadMore>
-                        <Box
-                          sx={{
-                            minWidth: "360px",
-                            maxWidth: "500px",
-                            display: "block",
-                          }}
-                        >
-                          <Styled.CardItems>
-                            <Typography
-                              sx={{ m: 0, whiteSpace: "nowrap" }}
-                              variant="CardHead"
-                              component={"h5"}
-                            >
-                              Name. &nbsp;
-                            </Typography>
-                            <Typography sx={{ ml: "20px" }} component={"span"}>
-                              {card.name}
-                            </Typography>
-                          </Styled.CardItems>
-                          <Styled.CardItems>
-                            <Typography
-                              sx={{ m: 0, whiteSpace: "nowrap" }}
-                              variant="CardHead"
-                              component={"h5"}
-                            >
-                              Surname. &nbsp;
-                            </Typography>
-                            <Typography sx={{ ml: "20px" }} component={"span"}>
-                              {card.surname}
-                            </Typography>
-                          </Styled.CardItems>
-                          <Styled.CardItems>
-                            <Typography
-                              sx={{ m: 0, whiteSpace: "nowrap" }}
-                              variant="CardHead"
-                              component={"h5"}
-                            >
-                              Email. &nbsp;
-                            </Typography>
-                            <Typography sx={{ ml: "20px" }} component={"span"}>
-                              {card.email}
-                            </Typography>
-                          </Styled.CardItems>
-                          <Styled.CardItems>
-                            <Typography
-                              sx={{ m: 0, whiteSpace: "nowrap" }}
-                              variant="CardHead"
-                              component={"h5"}
-                            >
-                              Phone. &nbsp;
-                            </Typography>
-                            <Typography sx={{ ml: "20px" }} component={"span"}>
-                              {card.phoneNumber}
-                            </Typography>
-                          </Styled.CardItems>
-                          <Styled.CardItems>
-                            <Typography
-                              sx={{ m: 0, whiteSpace: "nowrap" }}
-                              variant="CardHead"
-                              component={"h5"}
-                            >
-                              Description. &nbsp;
-                            </Typography>
-                            <Typography sx={{ ml: "20px" }} component={"span"}>
-                              {card.description}
-                            </Typography>
-                          </Styled.CardItems>
-                          <Styled.CardItems>
-                            <Typography
-                              sx={{ m: 0, whiteSpace: "nowrap" }}
-                              variant="CardHead"
-                              component={"h5"}
-                            >
-                              Date. &nbsp;
-                            </Typography>
-                            <Typography sx={{ ml: "20px" }} component={"span"}>
-                              {card.timestamp}
-                            </Typography>
-                          </Styled.CardItems>
-                        </Box>
-                      </ModalReadMore>
-                    )
-                  }
-                >
-                  <Box sx={{ maxWidth: "330px", display: "block" }}>
-                    <Styled.CardItems>
-                      <Typography
-                        sx={{ m: 0, whiteSpace: "nowrap" }}
-                        variant="CardHead"
-                        component={"h5"}
-                      >
-                        Name. &nbsp;
-                      </Typography>
-                      <Styled.UserData sx={{ ml: "20px" }} component={"span"}>
-                        {card.name}
-                      </Styled.UserData>
-                    </Styled.CardItems>
-                    <Styled.CardItems>
-                      <Typography
-                        sx={{ m: 0, whiteSpace: "nowrap" }}
-                        variant="CardHead"
-                        component={"h5"}
-                      >
-                        Surname. &nbsp;
-                      </Typography>
-                      <Styled.UserData sx={{ ml: "20px" }} component={"span"}>
-                        {card.surname}
-                      </Styled.UserData>
-                    </Styled.CardItems>
-                    <Styled.CardItems>
-                      <Typography
-                        sx={{ m: 0, whiteSpace: "nowrap" }}
-                        variant="CardHead"
-                        component={"h5"}
-                      >
-                        Email. &nbsp;
-                      </Typography>
-                      <Styled.UserData sx={{ ml: "20px" }} component={"span"}>
-                        {card.email}
-                      </Styled.UserData>
-                    </Styled.CardItems>
-                    <Styled.CardItems>
-                      <Typography
-                        sx={{ m: 0, whiteSpace: "nowrap" }}
-                        variant="CardHead"
-                        component={"h5"}
-                      >
-                        Phone. &nbsp;
-                      </Typography>
-                      <Styled.UserData sx={{ ml: "20px" }} component={"span"}>
-                        {card.phoneNumber}
-                      </Styled.UserData>
-                    </Styled.CardItems>
-                    <Styled.CardItems>
-                      <Typography
-                        sx={{ m: 0, whiteSpace: "nowrap" }}
-                        variant="CardHead"
-                        component={"h5"}
-                      >
-                        Description. &nbsp;
-                      </Typography>
-                      <Styled.UserData sx={{ ml: "20px" }} component={"span"}>
-                        {card.description}
-                      </Styled.UserData>
-                    </Styled.CardItems>
-                    <Styled.CardItems>
-                      <Typography
-                        sx={{ m: 0, whiteSpace: "nowrap" }}
-                        variant="CardHead"
-                        component={"h5"}
-                      >
-                        Date. &nbsp;
-                      </Typography>
-                      <Styled.UserData sx={{ ml: "20px" }} component={"span"}>
-                        {card.timestamp}
-                      </Styled.UserData>
-                    </Styled.CardItems>
-                  </Box>
-                </Styled.Card>
-              </Grid>
-            ))
-          : null}
-      </Styled.SubmitedForm>
-    </div>
-  );
->>>>>>> 12ac11f74c2b1f0d8386d22ee5795acf50afd965
 };
 
 export default Admin;
